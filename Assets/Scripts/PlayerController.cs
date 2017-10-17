52e518942e8adfdc26226e8d33a44b637088d364
Reyno VAN DER WESTHUIZEN <reyvan-d@e4r24p2.wethinkcode.co.za>
2017-10-17 10:06:48 +0200

    Teapot

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,7 +14,10 @@ public class PlayerController : MonoBehaviour {
 		mousePos = new Vector3 (0, 0, 0);
 		prevMousePos = new Vector3 (0, 0, 0);
 		playing = true;
-		transform.Rotate (0, 0, 90);
+		if (tag == "Stupid")
+			transform.Rotate (0, 0, 90);
+		else if (tag == "Easy")
+			transform.Rotate (0, 90, 90);
 	}
 
 	// Update is called once per frame
@@ -22,15 +25,28 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetKey(KeyCode.Mouse0)) {
 			mousePos = Input.mousePosition;
 		}
-		if (Mathf.Abs (transform.rotation.eulerAngles.z - solutionRotation.z) <= precision) {
+		if (Mathf.Abs (transform.rotation.eulerAngles.z - solutionRotation.z) <= precision &&
+			Mathf.Abs(transform.rotation.eulerAngles.x - solutionRotation.x) <= precision &&
+			Mathf.Abs(transform.rotation.eulerAngles.y - solutionRotation.y) <= precision && !(Input.GetKey(KeyCode.Mouse0))) {
 			playing = false;
 			if (transform.rotation.eulerAngles.z - solutionRotation.z <= 0.05f)
 				transform.Rotate (0, 0, 0.05f);
 			else
 				transform.Rotate (0, 0, -0.05f);
+			if (transform.rotation.eulerAngles.x - solutionRotation.x <= 0.05f)
+				transform.Rotate (0, 0, 0.05f);
+			else
+				transform.Rotate (0, 0, -0.05f);
+			if (transform.rotation.eulerAngles.y - solutionRotation.y <= 0.05f)
+				transform.Rotate (0, 0, 0.05f);
+			else
+				transform.Rotate (0, 0, -0.05f);
 		}
-		if (Input.GetKey (KeyCode.Mouse0) && playing) {
+		if (Input.GetKey (KeyCode.Mouse0) && playing && !(Input.GetKey(KeyCode.LeftControl))) {
 			transform.Rotate (0, 0, (mousePos.x - prevMousePos.x));
+		}
+		if ((tag == "Easy" || tag == "Normal") && Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.Mouse0)) {
+			transform.Rotate (0, (mousePos.y - prevMousePos.y), 0);
 		}
 		prevMousePos = mousePos;
 	}
