fec9ab0b473ad96fb5a6e4fbd550cd9450db1642
Reyno VAN DER WESTHUIZEN <reyvan-d@e4r24p2.wethinkcode.co.za>
2017-10-30 13:42:47 +0200

    Some menu stuff

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	private bool levelSelect;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ( Input.GetMouseButtonDown (0)){ 
+			RaycastHit hit; 
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+			if ( Physics.Raycast (ray,out hit,10.0f)){
+				if (hit.transform.name == "Normal") {
+
+				}
+				if (hit.transform.name == "Tester") {
+					GameObject.Find ("Normal").GetComponent<SignController> ().MoveUp ();
+					GameObject.Find ("Tester").GetComponent<SignController> ().MoveUp ();
+					GameObject.Find ("Exit").GetComponent<SignController> ().MoveUp ();
+					GameObject.Find ("Level1").GetComponent<SignController> ().MoveDown ();
+					GameObject.Find ("Level2").GetComponent<SignController> ().MoveDown ();
+					GameObject.Find ("Level3").GetComponent<SignController> ().MoveDown ();
+					levelSelect = true;
+				}
+				if (hit.transform.name == "Exit") {
+					Application.Quit ();
+				}
+				if (hit.transform.name == "Level1") {
+					SceneManager.LoadScene (1);
+				}
+				if (hit.transform.name == "Level2") {
+					SceneManager.LoadScene (2);
+				}
+				if (hit.transform.name == "Level3") {
+					SceneManager.LoadScene (3);
+				}
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.Escape) && levelSelect)
+		{
+			levelSelect = false;
+			GameObject.Find ("Normal").GetComponent<SignController> ().MoveDown ();
+			GameObject.Find ("Tester").GetComponent<SignController> ().MoveDown ();
+			GameObject.Find ("Exit").GetComponent<SignController> ().MoveDown ();
+			GameObject.Find ("Level1").GetComponent<SignController> ().MoveUp ();
+			GameObject.Find ("Level2").GetComponent<SignController> ().MoveUp ();
+			GameObject.Find ("Level3").GetComponent<SignController> ().MoveUp ();
+		}
+	}
+}
---
+++ Assets/Scripts/SignController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignController : MonoBehaviour {
+
+	private bool move;
+	private float stepsLeft, direction;
+
+	// Use this for initialization
+	void Start () {
+		direction = 0;
+		move = false;
+		stepsLeft = 0.0f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (move) {
+			stepsLeft = 10.0f;
+			move = false;
+		}
+		if (direction != 0) {
+			transform.Translate (0.0f, 0.0f, this.direction);
+			stepsLeft -= Mathf.Abs (this.direction);
+			if (stepsLeft <= 0.0f)
+				direction = 0;
+		}
+	}
+
+	public void MoveUp()
+	{
+		this.move = true;
+		this.direction = -0.5f;
+	}
+
+	public void MoveDown()
+	{
+		this.move = true;
+		this.direction = 0.5f;
+	}
+}
